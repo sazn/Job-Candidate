71866c432ffb092a955ffd770527b39c0ab76042
sazn <sazn_004@hotmail.com>
2024-06-11 13:43:58 +0545

    1. Added Candidate Controller and AddOrUpdateCandiate API

---
+++ JobCandidate.API/Controllers/CandidateController.cs
@@ -0,0 +1,30 @@
+﻿using JobCandidate.Domain.DomainClasses;
+using JobCandidate.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobCandidate.API.Controllers
+{    
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidateController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [Route("api/Candidate")]
+        [HttpPost]
+        public async Task<IActionResult> AddOrUpdateCandidate([FromBody] Candidate candidate)
+        {
+            if (candidate == null)
+            {
+                return BadRequest();
+            }
+
+            var result = await _candidateService.AddOrUpdateCandidateAsync(candidate);
+            return Ok(result);
+        }
+    }
+}
